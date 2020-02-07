3a74842b709a2e766e09aacbb06faf2c8972f2b7
Le Phu Hao <lephuhao24@gmail.com>
2020-02-07 10:13:16 +0700

    first commit

---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'alamofire_codewithchris' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for alamofire_codewithchris
+  pod “Alamofire”
+  pod 'AlamofireMapper'
+  pod 'ObjectMapper', '~> 3.1'
+  pod 'AlamofireObjectMapper', '~> 5.0'
+end
--- alamofire_codewithchris/ViewController.swift
+++ alamofire_codewithchris/ViewController.swift
@@ -7,12 +7,129 @@
 //
 
 import UIKit
+import Alamofire
+import AlamofireMapper
+import ObjectMapper
+import AlamofireObjectMapper
+
+/*
+ "userId": 1,
+ "id": 1,
+ "title": "delectus aut autem",
+ "completed": false
+ */
+
+class Todo: Decodable {
+    var userId: Int
+    var id: Int
+    var title: String
+    var completed: Bool
+}
+
+struct Production {
+    static let BASE_URL: String = "https://your_base_url.com/api/" // Thay thế bằng Base url mà bạn sử dụng ở đây
+}
+
+enum NetworkErrorType {
+    case API_ERROR
+    case HTTP_ERROR
+}
+
+enum APIRouter: URLRequestConvertible {
+    // =========== Begin define api ===========
+    case login(email: String, password: String)
+    case changePassword(pass: String, newPass: String, confirmNewPass: String)
+    
+    // =========== End define api ===========
+    
+    // MARK: - HTTPMethod
+    private var method: HTTPMethod {
+        switch self {
+        case .login, .changePassword:
+            return .post
+        }
+    }
+    
+    // MARK: - Path
+    private var path: String {
+        switch self {
+        case .login:
+            return "v1/user/login"
+        case .changePassword:
+            return "v1/user/change_password"
+        }
+    }
+    
+    // MARK: - Headers
+    private var headers: HTTPHeaders {
+        var headers = ["Accept": "application/json"]
+        switch self {
+        case .login:
+            break
+        case .changePassword:
+            headers["Authorization"] = getAuthorizationHeader()
+            break
+        }
+        
+        return headers;
+    }
+
+    // MARK: - Parameters
+    private var parameters: Parameters? {
+        switch self {
+        case .login(let email, let password):
+            return [
+                "email": email,
+                "password": password
+            ]
+        case .changePassword(let pass, let newPass, let confirmNewPass):
+            return[
+                "password": pass,
+                "new_password": newPass,
+                "new_password_confirmation": confirmNewPass
+            ]
+        }
+    }
+
+    // MARK: - URL request
+    func asURLRequest() throws -> URLRequest {
+        let url = try Production.BASE_URL.asURL()
+        
+        // setting path
+        var urlRequest: URLRequest = URLRequest(url: url.appendingPathComponent(path))
+        
+        // setting method
+        urlRequest.httpMethod = method.rawValue
+        
+        // setting header
+        for (key, value) in headers {
+            urlRequest.addValue(value, forHTTPHeaderField: key)
+        }
+        
+        if let parameters = parameters {
+            do {
+                urlRequest = try URLEncoding.default.encode(urlRequest, with: parameters)
+            } catch {
+                print("Encoding fail")
+            }
+        }
+        
+        return urlRequest
+    }
+    
+    private func getAuthorizationHeader() -> String? {
+        return "Authorization token"
+    }
+}
 
 class ViewController: UIViewController {
-
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
 
